bdd84dcf0289fdae5e38ce43b86c562e5a5b1da4
Dggalan <dggalan0@gmail.com>
2016-04-29 03:14:44 +0200

    Changes Config.groovy log4j configuration

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,3 +1,5 @@
+import org.apache.log4j.DailyRollingFileAppender
+
 // locations to search for config files that get merged into the main config;
 // config files can be ConfigSlurper scripts, Java properties files, or classes
 // in the classpath in ConfigSlurper format
@@ -97,9 +99,10 @@ environments {
 log4j = {
     // Example of changing the log pattern for the default console appender:
     //
-    //appenders {
-    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    //}
+    appenders {
+        console name:'stdout', layout: pattern(conversionPattern:'%d [%t] %-5p %c{2} %x - %m%n')
+
+    }
 
     error  'org.codehaus.groovy.grails.web.servlet',        // controllers
            'org.codehaus.groovy.grails.web.pages',          // GSP
@@ -112,4 +115,10 @@ log4j = {
            'org.springframework',
            'org.hibernate',
            'net.sf.ehcache.hibernate'
+    info    'grails.app'
+    root {
+        error 'file'
+        info 'file'
+        additivity = false
+    }
 }
